0a39a73576d8dc05c8e2346d3dc63b78faf22342
Bryce Kennedy <btken88@gmail.com>
2020-09-08 12:03:45 -0600

    Create tests for everything exercise.

---
+++ chapter5/everything.test.js
@@ -0,0 +1,20 @@
+const { every } = require('./flattening')
+
+
+test('should return true when all elements match the predicate', () => {
+  const trueArray = [5, 15, 20, 30]
+  function predicate(item) {
+    return item % 5 === 0
+  }
+
+  expect(every(trueArray, predicate)).toBe(true)
+})
+
+test('should return false when not all elements match the predicate', () => {
+  const falseArray = [5, 6, 15, 20]
+  function predicate(item) {
+    return item % 5 === 0
+  }
+
+  expect(every(falseArray, predicate)).toBe(false)
+})
